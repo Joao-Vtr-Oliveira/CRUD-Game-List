611f22e9a2f3938273c61ca24ebc36c28a3b6fa9
Joao V. <joao.vtr.oliveira@outlook.com>
2025-08-09 18:30:58 -0300

    feat: add the addCategory

--- CRUD-Game-List/Controllers/CategoryController.cs
+++ CRUD-Game-List/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using CRUD_Game_List.Business;
+using CRUD_Game_List.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,14 @@ namespace CRUD_Game_List.Controllers
 		public IActionResult Get()
 		{
 			return Ok(_categoryBusiness.GetCategories());
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody] Category category)
+		{
+			if (category == null) return BadRequest("Must provide a category.");
+			var cat = _categoryBusiness.AddCategory(category.Name);
+			return Ok(cat);
 		}
 	}
 }
