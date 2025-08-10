3b7d24c2f8f02cc5084075da48930544a8ff0e3a
Joao V. <joao.vtr.oliveira@outlook.com>
2025-08-10 09:43:04 -0300

    feat: add base of the game repository

---
+++ CRUD-Game-List/Repositories/GameRepository.cs
@@ -0,0 +1,37 @@
+﻿using CRUD_Game_List.Model;
+
+namespace CRUD_Game_List.Repositories
+{
+	public class GameRepository : IGameRepository
+	{
+		public Game AddGame(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DeleteGame(Game game)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ExistsByName(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Game? FindGameById(long id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Game> GetGames()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Game UpdateGame(Game game)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CRUD-Game-List/Repositories/IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using CRUD_Game_List.Model;
+
+namespace CRUD_Game_List.Repositories
+{
+	public interface IGameRepository
+	{
+		Game AddGame(string name);
+		List<Game> GetGames();
+		Game? FindGameById(long id);
+		Game UpdateGame(Game game);
+		void DeleteGame(Game game);
+		bool ExistsByName(string name);
+	}
+}
