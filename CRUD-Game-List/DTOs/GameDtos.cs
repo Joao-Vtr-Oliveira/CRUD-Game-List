2ba32d97366d7a1c40047a784b64f37e2a98410f
Joao V. <joao.vtr.oliveira@outlook.com>
2025-08-10 09:55:16 -0300

    feat: add a base DTO for game

---
+++ CRUD-Game-List/DTOs/GameDtos.cs
@@ -0,0 +1,13 @@
+﻿using CRUD_Game_List.Model;
+
+namespace CRUD_Game_List.DTOs
+{
+	public class GameDtos
+	{
+		public record GameCreateDto(string Title, long CategoryId, string ?Plataform, int ?Year);
+
+		public record GameUpdateDto(string Title, long CategoryId, string? Plataform, int? Year);
+
+		public record GameDto(long Id, string Title, long CategoryId, Category Cat, string? Plataform, int? Year);
+	}
+}
